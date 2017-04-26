e6bdb48a8cf26261df17e9052d706794cbb5438c
wangjing <wangjing>
2017-04-26 20:35:03 +0800

    first commit

--- app/src/main/java/com/wangjing/mystatusbar/Test1Activity.java
+++ app/src/main/java/com/wangjing/mystatusbar/Test1Activity.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 /**
@@ -11,10 +12,13 @@ import android.view.View;
  */
 
 public class Test1Activity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test1);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Test1Activity");
     }
 }
