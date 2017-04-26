f4e9d99659d1e1de16d34fe90f91937b0399b3a2
wangjing <wangjing>
2017-04-26 21:52:23 +0800

    first commit

--- app/src/main/java/com/wangjing/mystatusbar/Test2Activity.java
+++ app/src/main/java/com/wangjing/mystatusbar/Test2Activity.java
@@ -1,9 +1,11 @@
 package com.wangjing.mystatusbar;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 /**
@@ -11,13 +13,17 @@ import android.view.View;
  */
 
 public class Test2Activity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+//            getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
         setContentView(R.layout.activity_test2);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Test2Activity");
     }
+
 }
