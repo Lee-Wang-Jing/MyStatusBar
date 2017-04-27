49b76a22ee6ca53fece5937c5731b0b376469e50
wangjing <wangjing>
2017-04-28 01:42:06 +0800

    first commit

--- app/src/main/java/com/wangjing/mystatusbar/Test2Activity.java
+++ app/src/main/java/com/wangjing/mystatusbar/Test2Activity.java
@@ -1,5 +1,6 @@
 package com.wangjing.mystatusbar;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -7,6 +8,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * Created by Administrator on 2017/4/26.
@@ -21,9 +26,29 @@ public class Test2Activity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 //            getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
+
+//        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+//            setMiuiStatusBarDarkMode(this,true);
+//        }
         setContentView(R.layout.activity_test2);
         toolbar= (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Test2Activity");
     }
 
+
+    public static boolean setMiuiStatusBarDarkMode(Activity activity, boolean darkmode) {
+        Class<? extends Window> clazz = activity.getWindow().getClass();
+        try {
+            int darkModeFlag = 0;
+            Class<?> layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+            darkModeFlag = field.getInt(layoutParams);
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            extraFlagField.invoke(activity.getWindow(), darkmode ? darkModeFlag : 0, darkModeFlag);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
